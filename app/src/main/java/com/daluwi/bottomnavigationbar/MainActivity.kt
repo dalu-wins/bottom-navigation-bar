636acb21f70d81bec1ba19b573be4bf0e7b4f13d
Dalu Wins <daniil.wins@gmail.com>
2024-06-18 19:42:00 +0200

    Fixed issue #1

--- app/src/main/java/com/daluwi/bottomnavigationbar/MainActivity.kt
+++ app/src/main/java/com/daluwi/bottomnavigationbar/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.daluwi.bottomnavigationbar
 
 import android.os.Bundle
+import android.view.Window
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBarDefaults
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.surfaceColorAtElevation
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.toArgb
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -24,6 +30,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             BottomNavigationBarTheme {
+                val window: Window = this.window
+                window.navigationBarColor = getNavigationBarColor()
                 val navController = rememberNavController()
                 Scaffold(modifier = Modifier.fillMaxSize(),
                     bottomBar = { BottomNavigationBar(navController) }
@@ -35,22 +43,16 @@ class MainActivity : ComponentActivity() {
                     ) {
                         composable(
                             route = BottomNavigationItem.HISTORY.description,
-//                            enterTransition = { EnterTransition.None },
-//                            exitTransition = { ExitTransition.None }
                         ) {
                             HistoryScreen(navController)
                         }
                         composable(
-                            route = BottomNavigationItem.HOME.description,
-//                            enterTransition = { EnterTransition.None },
-//                            exitTransition = { ExitTransition.None }
+                            route = BottomNavigationItem.HOME.description
                         ) {
                             HomeScreen(navController)
                         }
                         composable(
                             route = BottomNavigationItem.SETTINGS.description,
-//                            enterTransition = { EnterTransition.None },
-//                            exitTransition = { ExitTransition.None }
                         ) {
                             SettingsScreen(navController)
                         }
@@ -58,5 +60,13 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+
+    @Composable
+    fun getNavigationBarColor(): Int {
+        return MaterialTheme
+            .colorScheme
+            .surfaceColorAtElevation(NavigationBarDefaults.Elevation)
+            .toArgb()
     }
 }
